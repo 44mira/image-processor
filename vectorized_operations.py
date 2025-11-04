3c65fe6c3240f6bb2fb54238beeee5cc9108cc99
44mira <legolaslada@gmail.com>
2025-11-04 23:54:59 +0800

    feat: get_histogram

--- vectorized_operations.py
+++ vectorized_operations.py
@@ -69,3 +69,23 @@ def to_grayscale(rgb_points: np.ndarray) -> np.ndarray:
     )
 
     return grayscale.astype(np.uint8)
+
+
+def get_histogram(channel: np.ndarray):
+    """
+    Get a histogram from a single channel matrix.
+
+    Args:
+        channel: 2-d array of values for the histogram
+
+    Returns:
+        (counts, bin_edges) tuple from np.histogram
+
+    Raises:
+        ValueError: if shape is invalid (not 2d)
+    """
+
+    if channel.ndim != 2:
+        raise ValueError("Input must be a 2d array.")
+
+    return np.histogram(channel, bins=256, range=(0, 255))
